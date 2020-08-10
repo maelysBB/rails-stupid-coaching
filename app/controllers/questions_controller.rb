0038e921170e82bd29cffb332b65ac970f963cbc
Maelys Bassilekin Blanc <maelysbassilekinblanc@gmail.com>
2020-08-10 12:47:26 +0200

    ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = if params[:name].downcase == 'i am going to work right now!'
+                ''
+              elsif params[:name].end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
